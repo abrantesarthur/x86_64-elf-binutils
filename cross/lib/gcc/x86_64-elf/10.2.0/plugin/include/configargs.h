4c117c8e4c38ad33922bed577e77db36a2282023
Arthur Abrantes <arthur.abrantes@wildlifestudios.com>
2020-09-21 19:01:15 -0300

    first commit

---
+++ cross/lib/gcc/x86_64-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-10.2.0/configure --target=x86_64-elf --prefix=/Users/arthur.abrantes/opt/cross --disable-nls --enable-languages=c,c++ --without-headers --with-libiconv-prefix=/usr/local/Cellar : (reconfigured) ../gcc-10.2.0/configure --target=x86_64-elf --prefix=/Users/arthur.abrantes/opt/cross --disable-nls --enable-languages=c,c++ --without-headers --with-libiconv-prefix=/usr/local/Cellar";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
